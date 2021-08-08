fdc7332bfc54e1248da05d104a3e70b3ac9d24d4
VishalUrankar-2211 <vvu060@gmail.com>
2021-08-08 10:55:46 +0530

    third day

---
+++ components/Map.js
@@ -0,0 +1,66 @@
+import { useState } from "react";
+import ReactMapGL, { Marker, Popup } from "react-map-gl";
+import getCenter from "geolib/es/getCenter";
+
+function Map({ searchResults }) {
+  const [selectedLocation, setSelectedLocation] = useState({});
+
+  const coordinates = searchResults.map((searchResult) => ({
+    longitude: searchResult.long,
+    latitude: searchResult.lat,
+  }));
+
+  const center = getCenter(coordinates);
+
+  const [viewport, setViewport] = useState({
+    width: "100%",
+    height: "100%",
+    latitude: center.latitude,
+    longitude: center.longitude,
+    zoom: 11,
+  });
+
+  return (
+    <ReactMapGL
+      mapStyle="mapbox://styles/vvu060/cks1t9ak952r817o5gukelnvw"
+      mapboxApiAccessToken={process.env.mapbox_key}
+      {...viewport}
+      onViewportChange={(nextViewPort) => setViewport(nextViewPort)}
+    >
+      {searchResults.map((result) => (
+        <div key={result.long}>
+          <Marker
+            longitude={result.long}
+            latitude={result.lat}
+            offsetLeft={-20}
+            offsetTop={-10}
+          >
+            <p
+              onClick={() => setSelectedLocation(result)}
+              className="cursor-pointer text-2xl animate-bounce"
+              aria-label="push-pin"
+            >
+              📌
+            </p>
+          </Marker>
+
+          {/* Popup on Click of Marker */}
+          {selectedLocation.long === result.long ? (
+            <Popup
+              onClose={() => setSelectedLocation({})}
+              closeOnClick={true}
+              longitude={result.long}
+              latitude={result.lat}
+            >
+              {result.title}
+            </Popup>
+          ) : (
+            false
+          )}
+        </div>
+      ))}
+    </ReactMapGL>
+  );
+}
+
+export default Map;
--- next.config.js
+++ next.config.js
@@ -2,4 +2,8 @@ module.exports = {
   images: {
     domains: ["links.papareact.com"],
   },
+  env: {
+    mapbox_key:
+      "pk.eyJ1IjoidnZ1MDYwIiwiYSI6ImNrczF0MmE3NDF3d3AzMnM3dnMycnJwaTgifQ.hZBaO_ZYU9xKlR1xiwfeCQ",
+  },
 };
